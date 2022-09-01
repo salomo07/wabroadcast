2cc10480c7699823a61394544f1578ccc68dbeb0
salomo08 <v.raja.sitompul@adira.co.id>
2022-09-01 10:37:52 +0700

    Init

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -66,6 +66,8 @@ class Chat extends CI_Controller {
             	else{
             		$data['namaevent']=$_GET['userdata']->nik.' - '.$val->from;
     				$this->sendtosocketInbound(json_encode($data));
+            		$this->sendtosocketBroadcast(json_encode($data));
+    				
             	}
 
         		$txt=$val->message->type=="IMAGE"?$val->message->caption:$val->message->text;
