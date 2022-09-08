d0da7af14d1dd3247dccfe175174e294bf7068a3
salomo08 <v.raja.sitompul@adira.co.id>
2022-09-08 11:40:00 +0700

    Init

--- application/controllers/Blast.php
+++ application/controllers/Blast.php
@@ -11,7 +11,6 @@ class Blast extends CI_Controller {
         // }
         // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'../../..');
 		// $dotenv->load();
-        echo base64_encode("Telecenter_WA:@Dira2021danny");
 		$_ENV['BASEURL_SOCKETIO']='wasocket.herokuapp.com';
         $uncontacted='Salam Sahabat {{1}},\n\nKami telah menerima pengajuan pembiayaan sahabat melalui *{{2}}*. Untuk proses selanjutnya, Petugas telesales akan menghubungi Anda untuk konfirmasi pembiayaan yang diajukan. \n\nMohon kesediaannya menginformasikan jadwal Anda dapat kami telepon kembali.\n\nSilakan klik *tombol di bawah*, petugas kami akan mencatat ulang jadwal telepon Anda.\n\nTerima Kasih Sahabat';
         $document='Salam Sahabat {{1}},\n\nTerima kasih telah mengajukan pembiayaan {{2}} di Adira Finance. \n\nSesuai komunikasi kita melalui telepon, mohon dokumen dapat dikirimkan melalui whatsapp ini berupa :\n{{3}}\n\nSilakan klik *tombol di bawah* ini, petugas kami akan memverifikasi kelengkapan dokumen Anda.\n\nTerima Kasih Sahabat';
